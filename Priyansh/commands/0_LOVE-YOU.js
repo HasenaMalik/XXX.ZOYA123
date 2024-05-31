be7ee33d818aac1c192fbad94a5c3dbfe498a0e8
HasenaMalik <159886409+HasenaMalik@users.noreply.github.com>
2024-05-31 10:33:34 +0530

    Update 0_LOVE-YOU.js

--- Priyansh/commands/0_LOVE-YOU.js
+++ Priyansh/commands/0_LOVE-YOU.js
@@ -20,7 +20,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 react.includes("love") ||
 react.includes("Love")) {
 		var msg = {
-				body: `𝑰 𝑳𝑶𝑽𝑬 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑴𝑬𝑹𝑰 𝑱𝑨𝑨𝑵 🙈`,attachment: fs.createReadStream(__dirname + `/AADI/love.gif`)
+				body: `𝑰 𝑳𝑶𝑽𝑬 𝒀𝑶𝑼 𝑻𝑶𝑶 𝑴𝑬𝑹𝑰 𝑱𝑨𝑨𝑵 🙈`,attachment: fs.createReadStream(__dirname + `/ZOYA/love.gif`)
 			}
 			api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🙈", event.messageID, (err) => {}, true)
