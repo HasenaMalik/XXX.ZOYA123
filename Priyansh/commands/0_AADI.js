5e8f3f9af5a2983eebe1130905acbfdcebf91bf2
HasenaMalik <159886409+HasenaMalik@users.noreply.github.com>
2024-06-06 10:02:44 +0530

    Update 0_AADI.js

--- Priyansh/commands/0_AADI.js
+++ Priyansh/commands/0_AADI.js
@@ -14,8 +14,8 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
 	var { threadID, messageID } = event;
 	let react = event.body.toLowerCase();
 	if(react.includes("owner ") ||
-     react.includes("Zoya") || 
-react.includes("Zoya")) {
+     react.includes("ZOYA") || 
+react.includes("@️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️")) {
 		var msg = {
 				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆 ★\n\n✦🌸===『*★🌸===ज़ोया बाबू✦\n\n★★᭄𝐈𝐍𝐒𝐓𝐀𝐆𝐑𝐀𝐌 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐀𝐁𝐔 𝐊𝐀 :\n\n✦https://www.instagram.com/invites/contact/?i=1pxed7plmiw3w&utm_content=us4gn9p=  ✦ \n𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 𝐋𝐈𝐍𝐊 𝐌𝐄𝐑𝐄 𝐁𝐀𝐁𝐔 𝐊𝐀 😁\n 𝐌𝐄𝐑𝐄 𝐁𝐎𝐒𝐒 𝐊𝐀 𝐋𝐔𝐊 😁?https://www.facebook.com/profile.php?id=61559376917911&mibextid=ZbWKwL`)
 			}
